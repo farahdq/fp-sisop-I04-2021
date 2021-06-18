fe2e191d1a3a7042679541826a8664156c8edf85
farahdq <73812417+farahdq@users.noreply.github.com>
2021-06-18 21:57:52 +0700

    Update program_client.c

--- client/program_client.c
+++ client/program_client.c
@@ -1,6 +1,7 @@
-#include <stdio.h>	
-#include <string.h>	
-#include <sys/socket.h>	
-#include <arpa/inet.h>	
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#define PORT 8080
